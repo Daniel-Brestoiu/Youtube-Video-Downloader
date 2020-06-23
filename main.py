1e6fa304c6586376cf5d9b6ee4d67249664b07e8
Daniel-Brestoiu <47580794+Daniel-Brestoiu@users.noreply.github.com>
2020-06-22 21:06:42 -0400

    Basic youtube interaction created. Tested out searching on youtube.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-
+import googleapiclient
+from googleapiclient.discovery import build
 
 API_key = 'AIzaSyBs1qMkQYzS4Vr2oCBYHOfx_TTcM6xYGUk'
+
+youtube = build("youtube", "v3", developerKey= API_key)
+
+request = youtube.search().list(q= "Music", part = "snippet", type = "video")      #type --> googleapiclient.http.HttpRequest
+result = request.execute()
+
+print()
+for item in result["items"]:
+    print(item['snippet']["title"])
+    print()

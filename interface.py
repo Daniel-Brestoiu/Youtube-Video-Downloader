77e88e907be2876f7edde5388e70c58426c206b3
Daniel-Brestoiu <47580794+Daniel-Brestoiu@users.noreply.github.com>
2020-06-29 22:46:29 -0400

    Initial interface creation.

--- interface.py
+++ interface.py
@@ -5,4 +5,24 @@ import ffmpeg_script
 import typing
 from typing import List, Tuple
 
-#Now just re-learn tkinter, simple.
+#My guy what is my plan for the design here
+
+root = tkinter.Tk()
+
+def init_screen():
+    """Creates the general screen of app"""
+
+    root.title("Youtube Video Downloader")
+    #Making Main Screen
+
+    root.geometry("500x500+650+250")
+    root.minsize(width = 500, height = 500)
+    root.maxsize(width = 500, height = 500)
+    root["background"] = "#c4c2ff"
+
+    #Setting main screen size, relative initial position, background colour
+
+if __name__ == "__main__":
+    init_screen()
+
+    root.mainloop()
